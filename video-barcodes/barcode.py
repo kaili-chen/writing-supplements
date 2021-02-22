81f1c6f4118aa6d46a080c2d33ef710070c691d3
kaili <26805525+kaili-chen@users.noreply.github.com>
2021-02-22 22:17:06 +0800

    added basic try except

--- video-barcodes/barcode.py
+++ video-barcodes/barcode.py
@@ -1,6 +1,9 @@
 from extract_video_frames import *
 from make_barcode import *
 
-frames_dir = extract_video_frames('opening.mp4')
-frames_dir='opening frames'
+try:
+    frames_dir = extract_video_frames('opening.mp4')
+except FileExistsError:
+    # extract video frames not run, dir already exists
+    frames_dir='opening frames'
 barcode = make_barcode(frames_dir, bar_width=2, dims=(300,300))
